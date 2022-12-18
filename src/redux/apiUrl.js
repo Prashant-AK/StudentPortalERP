aa3d7f1d299034c492f3937e98dfce7898882a92
PRASHANT SINGH <ps.prashantsingh.98@gmail.com>
2022-12-18 22:41:59 +0530

    changes in base url

--- src/redux/apiUrl.js
+++ src/redux/apiUrl.js
@@ -1,4 +1,4 @@
-// export const BASE_URL =
-// 'http://ec2-43-204-24-149.ap-south-1.compute.amazonaws.com';
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL =
+  'http://ec2-43-204-24-149.ap-south-1.compute.amazonaws.com';
+// export const BASE_URL = 'http://localhost:4000';
 // localhost:4000/api/student/create-student
